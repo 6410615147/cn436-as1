c573441a9fab556d1d7a021be5148b91d350ef74
6410615147 <6410615147@student.tu.ac.th>
2024-01-27 18:06:00 +0700

    Initial Commit

---
+++ NumberGuessing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberGuessing
+//
+//  Created by Babypowder on 27/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
